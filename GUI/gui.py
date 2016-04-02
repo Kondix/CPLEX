34219b0e75c4709d991978587d6fdcfd06212e2e
Kondix <kondziox@gmail.com>
2016-04-02 12:47:03 +0200

    [GUI] podzial na frame, dodanie mozliwosci wyboru betsow

--- GUI/gui.py
+++ GUI/gui.py
@@ -7,52 +7,99 @@ class GUI(tk.Frame, Data):
 	def __init__(self, master=None):
 		tk.Frame.__init__(self, master)
 		self.pack()
+
+		self.buttonFrame = tk.Frame(self)
+		self.buttonFrame.pack()
+
+		self.dataFrame = tk.Frame(self)
+		self.dataFrame.pack(pady = 5)
+
+		self.presentationFrame = tk.Frame(self)
+		self.presentationFrame.pack(side = tk.LEFT)
+
+		self.betsFrame = tk.Frame(self)
+		self.betsFrame.pack(side = tk.LEFT, fill = tk.X, padx = 5)		
+
 		self.createWidgets()
 		self.m_data = Data()
 		self.riskOrGainBool = 0
 		self.betsVector = []
 
 	def createWidgets(self):
-		self.start = tk.Button(self)
+		self.__initButtonFrame()
+		self.__initDataFrame()
+		self.__initPresentationFrame()
+		self.__initBetsFrame()
+
+	def __initButtonFrame(self):
+		self.start = tk.Button(self.buttonFrame)
 		self.start["text"] = "Pobierz"
 		self.start["command"] = self.downloadParse
-		self.start.pack(side=tk.TOP, fill = tk.BOTH)
+		self.start.pack(side=tk.LEFT, fill = tk.BOTH)
 
-		self.start = tk.Button(self)
+		self.start = tk.Button(self.buttonFrame)
 		self.start["text"] = "Oblicz"
 		self.start["command"] = self.startProcessing
-		self.start.pack(side=tk.TOP, fill = tk.BOTH)
+		self.start.pack(side=tk.LEFT, fill = tk.BOTH)
 
-		self.riskOrGain = tk.Checkbutton(self)
+	def __initDataFrame(self):
+		self.riskOrGain = tk.Checkbutton(self.dataFrame)
 		self.riskOrGain["text"] = "Zaznacz aby wybrać obliczanie ryzyka.\nWyłączony oznacza kalkulacje zysku."
 		self.riskOrGain["command"] = self.toggleRiskOrGain
-		self.riskOrGain.pack()
+		self.riskOrGain.pack(side = tk.TOP)
 
-		self.mainVar = tk.Entry(self)
+		self.mainVar = tk.Entry(self.dataFrame)
 		self.mainVar.insert(0, "Ryzyko/zysk")
-		self.mainVar.pack(side = tk.LEFT)
+		self.mainVar.pack(side = tk.BOTTOM)
 
-		self.budget = tk.Entry(self)
+		self.budget = tk.Entry(self.dataFrame)
 		self.budget.insert(0, "Kwota")
-		self.budget.pack(side = tk.LEFT)
+		self.budget.pack(side = tk.BOTTOM)
 
-		self.maxDay = tk.Entry(self)
+		self.maxDay = tk.Entry(self.dataFrame)
 		self.maxDay.insert(0, "Ilosc dni")
-		self.maxDay.pack(side = tk.LEFT)
+		self.maxDay.pack(side = tk.BOTTOM)
 
-		self.minBet = tk.Entry(self)
+		self.minBet = tk.Entry(self.dataFrame)
 		self.minBet.insert(0, "minBet")
-		self.minBet.pack(side = tk.LEFT)
+		self.minBet.pack(side = tk.BOTTOM)
 
-		self.maxBet = tk.Entry(self)
+		self.maxBet = tk.Entry(self.dataFrame)
 		self.maxBet.insert(0, "maxBet")
-		self.maxBet.pack(side = tk.LEFT)
+		self.maxBet.pack(side = tk.BOTTOM)
 
-		self.scrollbar = tk.Scrollbar(root)
+	def __initPresentationFrame(self):
+		self.scrollbar = tk.Scrollbar(self.presentationFrame)
 		self.scrollbar.pack(side = tk.LEFT, fill=tk.Y)
-		self.betNamesList = tk.Listbox(root, yscrollcommand = self.scrollbar.set)
-		self.betNamesList.pack(side = tk.LEFT, fill = tk.BOTH)
-		self.scrollbar.config(command = self.betNamesList.yview)
+		self.betNamesList = tk.Listbox(self.presentationFrame, yscrollcommand = self.scrollbar.set)
+		self.betNamesList.pack(side = tk.LEFT, fill = tk.Y)
+		self.scrollbar.config(command = self.betNamesList.yview),
+
+	def __initBetsFrame(self):
+		self.bet1 = tk.Button(self.betsFrame)
+		self.bet1["text"] = "bet1"
+		self.bet1["command"] = self.startProcessing
+		self.bet1.pack(side=tk.LEFT, fill = tk.BOTH)
+
+		self.bet2 = tk.Button(self.betsFrame)
+		self.bet2["text"] = "bet2"
+		self.bet2["command"] = self.startProcessing
+		self.bet2.pack(side=tk.LEFT, fill = tk.BOTH)
+
+		self.bet3 = tk.Button(self.betsFrame)
+		self.bet3["text"] = "bet3"
+		self.bet3["command"] = self.startProcessing
+		self.bet3.pack(side=tk.LEFT, fill = tk.BOTH)
+
+		self.bet4 = tk.Button(self.betsFrame)
+		self.bet4["text"] = "bet4"
+		self.bet4["command"] = self.startProcessing
+		self.bet4.pack(side=tk.LEFT, fill = tk.BOTH)
+
+		self.bet5 = tk.Button(self.betsFrame)
+		self.bet5["text"] = "bet5"
+		self.bet5["command"] = self.startProcessing
+		self.bet5.pack(side=tk.LEFT, fill = tk.BOTH)
 
 	def downloadParse(self):
 		print("zaczynam pobieranie")
